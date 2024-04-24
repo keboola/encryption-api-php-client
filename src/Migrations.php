0a914ea3368598fa90603597098e6ff73bd1768c
Roman Pištěk <roman.pistek@keboola.com>
2024-04-24 20:02:10 +0200

    Add Migrations client

---
+++ src/Migrations.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\EncryptionApiClient;
+
+class Migrations extends Common
+{
+    public function __construct(string $sapiToken, array $config)
+    {
+        parent::__construct(['X-StorageApi-Token' => $sapiToken], $config);
+    }
+
+    public function migrateConfiguration(
+        string $sourceStorageApiToken,
+        string $destinationStack,
+        string $destinationStorageApiToken,
+        string $componentId,
+        string $configId,
+        string $branchId,
+        bool $dryRun = false,
+    ): array {
+        $queryParams = [];
+        if ($dryRun) {
+            $queryParams['dry-run'] = 'true';
+        }
+
+        $url = 'migrate-configuration';
+        if (count($queryParams) > 0) {
+            $url .= '?' . http_build_query($queryParams);
+        }
+
+        return $this->apiPost($url, [
+            'sourceStorageApiToken' => $sourceStorageApiToken,
+            'destinationStack' => $destinationStack,
+            'destinationStorageApiToken' => $destinationStorageApiToken,
+            'componentId' => $componentId,
+            'configId' => $configId,
+            'branchId' => $branchId,
+        ]);
+    }
+}
